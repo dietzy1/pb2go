f2d6499406899c8503b933b05981ceb32d74db7b
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-07-29 22:27:55 +0200

    Major restructure: alot of boilerplate was removed

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -2,177 +2,72 @@ package codegen
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
 
+	"github.com/dietzy1/pb2go/parser"
 	"github.com/dietzy1/pb2go/templates"
 )
 
-const (
-	serverFolderName = "server"
-	dataStoreName    = "datastore"
-	domainFolderName = "domain"
-	protoFolderName  = "proto"
+func Run(proto parser.Service) error {
 
-	bufFileName    = "buf.yaml"
-	bufGenFileName = "buf.gen.yaml"
-)
-
-func createRootFolder(serviceName, githubName, fileName string) error {
-
-	err := os.Mkdir(serviceName, 0755)
+	//Create new builder object should only be instantiated once
+	builder, err := templates.NewBuilder(proto)
 	if err != nil {
-		return fmt.Errorf("error: Unable to create the root folder: %v", err)
+		return err
 	}
 
-	//Go into the root folder
-	if err = os.Chdir(serviceName); err != nil {
-		return fmt.Errorf("error: Unable to change directory: %v", err)
+	//Create root folder
+	if err := os.MkdirAll(proto.ServiceName, 0755); err != nil {
+		return err
 	}
 
-	//Create the server folder
-	if err = createServerFolder(); err != nil {
-		return fmt.Errorf("error: Unable to create the %s folder: %v", serverFolderName, err)
+	//Change directory to the root folder
+	if err := os.Chdir(proto.ServiceName); err != nil {
+		return err
 	}
 
-	//Create the repository folder
-	if err = createDatastoreFolder(); err != nil {
-		return fmt.Errorf("error: Unable to create the %s folder: %v", dataStoreName, err)
+	//Generate folders and files
+	if err := generateAndReturn(builder); err != nil {
+		return err
 	}
 
-	//Create the domain folder
-	if err = createDomainFolder(); err != nil {
-		return fmt.Errorf("error: Unable to create the %s folder: %v", domainFolderName, err)
+	//Generate files
+	if err := generateProtobuffers(proto.FileName, proto.GithubName, proto.ServiceName); err != nil {
+		return err
 	}
 
-	//Create the proto folder
-	if err = createProtoFolder(fileName, githubName, serviceName); err != nil {
-		return fmt.Errorf("error: Unable to create the %s folder: %v", protoFolderName, err)
+	//instantiate go mod file
+	if err := goModInit(proto.GithubName, proto.ServiceName); err != nil {
+		return err
 	}
 
-	return nil
-}
-
-func createServerFolder() error {
-
-	if err := os.Mkdir(serverFolderName, 0755); err != nil {
-		return fmt.Errorf("error: Unable to create the %s folder: %v", serverFolderName, err)
-	}
-
-	//Go into the server folder
-	if err := os.Chdir(serverFolderName); err != nil {
-		return fmt.Errorf("error: Unable to change directory: %v", err)
-	}
-
-	//Do some stuff here
-
-	//Go back to the root folder
-	if err := os.Chdir(".."); err != nil {
-		return fmt.Errorf("error: Unable to change directory: %v", err)
-	}
-
-	return nil
-}
-
-func createDatastoreFolder() error {
-
-	if err := os.Mkdir(dataStoreName, 0755); err != nil {
-		return fmt.Errorf("error: Unable to create the %s folder: %v", dataStoreName, err)
-	}
-
-	//Go into the repository folder
-	if err := os.Chdir(dataStoreName); err != nil {
-		return fmt.Errorf("error: Unable to change directory: %v", err)
-	}
-
-	//Do some stuff here
-
-	//Go back to the root folder
-	if err := os.Chdir(".."); err != nil {
-		return fmt.Errorf("error: Unable to change directory: %v", err)
-	}
-
-	return nil
-}
-
-func createDomainFolder() error {
-
-	if err := os.Mkdir(domainFolderName, 0755); err != nil {
-		return fmt.Errorf("error: Unable to create the %s folder: %v", domainFolderName, err)
-	}
-
-	//Go into the domain folder
-	if err := os.Chdir(domainFolderName); err != nil {
-		return fmt.Errorf("error: Unable to change directory: %v", err)
-	}
-
-	//Do some stuff here
-
-	//Go back to the root folder
-	if err := os.Chdir(".."); err != nil {
-		return fmt.Errorf("error: Unable to change directory: %v", err)
-	}
-
-	return nil
-}
-
-func createProtoFolder(fileName, githubName, serviceName string) error {
-
-	if err := os.Mkdir(protoFolderName, 0755); err != nil {
-		return fmt.Errorf("error: Unable to create the %s folder: %v", protoFolderName, err)
-	}
-
-	//Go into the proto folder
-	if err := os.Chdir(protoFolderName); err != nil {
-		return fmt.Errorf("error: Unable to change directory: %v", err)
-	}
-	//Call generator functions to create the 2 yaml files
-
-	//Create file for the buf.gen.yaml
-	file, err := os.Create(bufGenFileName)
-	if err != nil {
-		return fmt.Errorf("error: Unable to create the %s file: %v", bufGenFileName, err)
-	}
-
-	err = templates.GenerateGenYaml(file)
-	if err != nil {
-		return fmt.Errorf("error: Unable to generate the %s file: %v", bufGenFileName, err)
-	}
-
-	/* 	_, err = file.WriteString(yaml)
-	   	if err != nil {
-	   		return fmt.Errorf("error: Unable to write to the %s file: %v", bufGenFileName, err)
-	   	} */
-	file.Close()
-
-	//Create file for the buf.yaml
-	file, err = os.Create(bufFileName)
-	if err != nil {
-		return fmt.Errorf("error: Unable to create the %s file: %v", bufFileName, err)
-	}
-
-	err = templates.GenerateYaml(file)
-	if err != nil {
-		return fmt.Errorf("error: Unable to generate the %s file: %v", bufFileName, err)
-	}
-
-	/* _, err = file.WriteString(yaml)
-	if err != nil {
-		return fmt.Errorf("error: Unable to write to the %s file: %v", bufFileName, err)
+	/* if err := installDependencies(); err != nil {
+		return fmt.Errorf("error: Unable to install the dependencies: %v", err)
 	} */
-	defer file.Close()
+
+	return nil
+}
+
+func generateProtobuffers(fileName, githubName, serviceName string) error {
+	//Go into the proto folder
+	if err := os.Chdir("proto"); err != nil {
+		return fmt.Errorf("error: Unable to change directory: %v", err)
+	}
 
 	//Create 2 directories v1 and v2
-	if err = os.MkdirAll(fileName+"/v1", 0755); err != nil {
+	if err := os.MkdirAll(fileName+"/v1", 0755); err != nil {
 		return fmt.Errorf("error: Unable to create the v1 folder: %v", err)
 	}
 
-	if err = os.Chdir(fileName + "/v1"); err != nil {
+	if err := os.Chdir(fileName + "/v1"); err != nil {
 		return fmt.Errorf("error: Unable to change directory: %v", err)
 	}
 
 	//Copy the proto file into the v1 folder
-	if err = copyFile("../../../../"+fileName+".proto", fileName+".proto"); err != nil {
+	if err := copyFile("../../../../"+fileName+".proto", fileName+".proto"); err != nil {
 		return fmt.Errorf("error: Unable to copy the proto file: %v", err)
 	}
 
@@ -180,44 +75,80 @@ func createProtoFolder(fileName, githubName, serviceName string) error {
 	//option go_package = "github.com/dietzy1/chatapp/services/chatroom/proto/chatroom/v1;chatroomv1";
 	optionPackage := fmt.Sprintf("option go_package = \"github.com/%s/%s/proto/%s/v1;%sv1\";", githubName, fileName, fileName, fileName)
 
-	if err = insertSnippetInFile(fileName+".proto", optionPackage); err != nil {
+	if err := insertSnippetInFile(fileName+".proto", optionPackage); err != nil {
 		return fmt.Errorf("error: Unable to insert the snippet in the proto file: %v", err)
 	}
 
 	//I need to go back to directories
-	if err = os.Chdir("../.."); err != nil {
+	if err := os.Chdir("../../"); err != nil {
 		return fmt.Errorf("error: Unable to change directory: %v", err)
 	}
 
 	//Here we need to generate the go files from the proto file
 	//We need to call the protoc command
 	command := exec.Command("buf", "generate")
-	_, err = command.Output()
+	_, err := command.Output()
 	if err != nil {
 		return fmt.Errorf("error: Unable to generate the go files: %v", err)
 	}
 
-	//Go back to the root folder
-	if err := os.Chdir(".."); err != nil {
+	if err := os.Chdir("../"); err != nil {
 		return fmt.Errorf("error: Unable to change directory: %v", err)
 	}
 
-	//Create
-
-	//templating.GenerateDomain()
-
 	return nil
+
 }
 
-// Entry function for the codegen package
-func Run1(serviceName, githubName, fileName string) error {
+// GenerateAndReturn creates a directory, navigates to that directory,
+// calls the generation function, and returns to the prior directory.
+func generateAndReturn(builder *templates.Builder) error {
 
-	if err := createRootFolder(serviceName, githubName, fileName); err != nil {
-		return fmt.Errorf("error: Unable to create the root folder: %v", err)
-	}
+	//Get directories we are generating
+	directories := templates.DeclaredDirectories()
 
-	if err := goModInit(githubName, serviceName); err != nil {
-		return fmt.Errorf("error: Unable to create the go.mod file: %v", err)
+	for _, dir := range directories {
+
+		// Get the current working directory
+		prevDir, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		// Create the target directory
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			return err
+		}
+
+		// Change to the target directory
+		if err := os.Chdir(dir); err != nil {
+			return err
+		}
+
+		for _, v := range builder.Templates[dir] {
+			fmt.Println(v.Name())
+
+			fileName := strings.TrimSuffix(v.Name(), ".tmpl")
+
+			// create a file object
+			file, err := os.Create(fileName)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			//remove 5 characters from the end of the string
+
+			if err := builder.Build(file, dir, v.Name()); err != nil {
+				return err
+			}
+			log.Println("Generated", v.Name())
+		}
+		if err := os.Chdir(prevDir); err != nil {
+			fmt.Printf("error: Unable to change directory: %v", err)
+		}
+		log.Println("Returned to", prevDir)
+
 	}
 
 	return nil
